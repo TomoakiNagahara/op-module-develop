75e5020ad9b7009ebfae2e7fc47a16c79b569994
Tomoaki imac5k-2017 <1668339+TomoakiNagahara@users.noreply.github.com>
2019-12-27 12:41:38 +0900

    New: selftest

---
+++ selftest/action.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * module-develop:/selftest/action.php
+ *
+ * @created   2019-01-30
+ * @version   1.0
+ * @package   module-develop
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/* @var $app OP\UNIT\App */
+
+D( require_once('test.php') );
+D( require_once('test.php') );
