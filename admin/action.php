4ce703438056407c9708283634f412083ad6273a
Tomoaki mbp13r-2015 <tomoaki.nagahara@gmail.com>
2020-08-25 18:32:21 +0900

    New: admin > README.md, action.php, list.php

---
+++ admin/action.php
@@ -0,0 +1,52 @@
+<?php
+/** op-module-develop:/admin/action.php
+ *
+ * @created   2019-01-30
+ * @copied    2020-08-25   Copy from op-module-develop:/selftest/action.php
+ * @version   1.0
+ * @package   op-module-develop
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+$develop_root = ConvertURL('develop:/admin/');
+
+//	...
+$args = Unit('Router')->Args();
+
+?>
+<?php if( $list = include('list.php') ): ?>
+<section>
+[
+	<?php foreach( $list as $name ): ?>
+	<span class="menu"><a href="<?= $develop_root . $name ?>"><?= $name ?></a></span>
+	<?php endforeach; ?>
+]
+</section>
+<?php else: ?>
+<p style="margin: 1em;">No unit has an admin directory.</p>
+<?php endif; ?>
+<?php
+//	...
+if(!$name = $args[1] ?? null ){
+	echo '<pre>';
+	include('README.md');
+	echo '</pre>';
+	return;
+}
+
+//	Build meta path.
+$meta_path = "asset:/unit/{$name}/admin/action.php";
+
+//	...
+if( file_exists( ConvertPath($meta_path) ) ){
+	Template($meta_path);
+}else{
+	D("There is no action.php file in the admin directory. ($meta_path)");
+}
