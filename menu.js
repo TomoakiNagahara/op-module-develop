fdf48194c2a95725ea0f3c390c31802912b900e6
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2023-02-02 13:16:31 +0900

    New: menu.js

---
+++ menu.js
@@ -0,0 +1,18 @@
+
+/** op-module-develop:/menu.js
+ *
+ * @created    2023-01-30
+ * @version    1.0
+ * @package    op-module-develop
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+//	...
+document.addEventListener('DOMContentLoaded', function(){
+	document.querySelectorAll('section.menu .fold .switch').forEach((node) => {
+		node.addEventListener('click', (e) => {
+			let items = e.target.parentNode.querySelector('.items');
+				items.style.display = items.style.display ? '' : 'none';
+		});
+	});
+});
