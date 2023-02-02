f6ad0fa64dab58fc63ea02cff28948b48e8f79eb
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2023-02-02 13:20:16 +0900

    New: reference.php

---
+++ reference.php
@@ -0,0 +1,68 @@
+<?php
+/** op-module-develop:/reference.php
+ *
+ * @created   2023-02-01
+ * @version   1.0
+ * @package   op-module-develop
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** declare
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+$args = OP::Router()->Args();
+
+//	...
+switch( $meta = $args[1] ?? '' ){
+	case '':
+		$path = 'git:/README';
+		break;
+
+	case 'core':
+	case 'asset':
+		if( empty($args[2]) ){
+			$path = $meta === 'core' ? "{$meta}:/README": "{$meta}:/reference/README";
+		}else{
+			$path = "{$meta}:/reference/{$args[2]}";
+		}
+		break;
+
+	case 'unit':
+		if( empty($args[3]) ){
+			$path = "{$meta}:/{$args[2]}/README";
+		}else{
+			$path = "{$meta}:/{$args[2]}/reference/{$args[3]}";
+		}
+		break;
+
+	default:
+		foreach( OP::Unit('Git')->SubmoduleConfig() as $config ){
+			if(!strpos($config['url'], $args[1]) ){
+				continue;
+			}
+			if(!strpos($config['url'], $args[2]) ){
+				continue;
+			}
+			break;
+		}
+		if( empty($args[3]) ){
+			$path = "git:/{$config['path']}/README";
+		}else{
+			$path = "git:/{$config['path']}/reference/{$args[3]}";
+		}
+	break;
+}
+
+//	...
+if( $path ?? null ){
+	OP::Markdown($path.'.md');
+}
