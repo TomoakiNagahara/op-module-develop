931049b1d8ada83debe35b2b1aa4aeeb3f4a457a
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-02-15 13:19:10 +0900

    New: testcase > XSS.md, XSS.php

---
+++ testcase/XSS.php
@@ -0,0 +1,22 @@
+<?php
+/** op-module-develop:/testcase/XSS.php
+ *
+ * @created   2024-02-14
+ * @version   1.0
+ * @package   op-module-develop
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** declare
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP\MODULE\DEVELOP;
+
+//	...
+OP()->Markdown('XSS.md');
