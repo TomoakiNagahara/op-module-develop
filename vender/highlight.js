7caa164ec5a5e400cd07923310b1ec9d767d4b53
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-05-29 07:55:36 +0900

    Fix: vender > highlight.js | Loader.

---
+++ vender/highlight.js
@@ -0,0 +1,11 @@
+//<?php
+/** op-module-develop:/vender/highlight.js
+ *
+ * @created    2024-05-21
+ * @version    1.0
+ * @package    op-module-develop
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+echo "\n";
+echo file_get_contents(__DIR__.'/highlight/11.7.0.min.js');
