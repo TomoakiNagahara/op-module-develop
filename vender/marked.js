1fdf8b602dfbaaf265f2f4451adc6c15fd8fb2ff
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-05-29 07:55:56 +0900

    Fix: vender > marked.js | Loader.

---
+++ vender/marked.js
@@ -0,0 +1,11 @@
+//<?php
+/** op-module-develop:/vender/marked.js
+ *
+ * @created    2024-05-21
+ * @version    1.0
+ * @package    op-module-develop
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+echo "\n";
+echo file_get_contents(__DIR__.'/marked/12.0.2.min.js');
