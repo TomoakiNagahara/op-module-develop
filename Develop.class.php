d3f4e9fe20d7c71e129b8c850ca1f4e53bc1a3a5
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-07-29 20:46:08 +0900

    New: Develop.class.php

---
+++ Develop.class.php
@@ -0,0 +1,30 @@
+<?php
+/**	op-module-develop:/Develop.class.php
+ *
+ * @created    2025-07-29
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	declare
+ *
+ */
+declare(strict_types=1);
+
+/**	namespace
+ *
+ */
+namespace OP\MODULE;
+
+/**	Develop
+ *
+ * @created    2025-07-29
+ * @version    1.0
+ * @package    op-module-develop
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+class Develop
+{
+
+}
