421dd8041f5691ece9ca18990501097c1e15625b
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-12-09 21:28:47 +0900

    New: server.php

---
+++ server.php
@@ -0,0 +1,21 @@
+<?php
+/**	op-module-develop:/server.php
+ *
+ * @created    2025-12-09
+ * @license    Apache-2.0
+ * @package    op-module-develop
+ * @copyright  (C) 2025 Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=0);
+
+/**	Namespace
+ *
+ */
+namespace OP\DEVELOP;
+
+//	...
+D( $_SERVER );
